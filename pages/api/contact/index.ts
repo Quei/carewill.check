be9f38db963b5e85dfa873bdf9b471f70668a558
Quei <github@quei.org>
2021-08-29 02:34:54 +0900

    Init dev 27 check mail variables

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -4,40 +4,48 @@ import { hauteCouture } from './template';
 
 const contact = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const transporter = createTransport({
-      host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.MAIL_ADDRESS_WEB,
-        pass: process.env.MAIL_PASS_WEB,
-      },
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false,
-      },
-    });
-    let subject = 'お問い合わせ';
-    let text = '';
-    let to = process.env.MAIL_ADDRESS_CONTACT;
-    if (req.body?.type === 'haute-couture') {
-      const mailOption = hauteCouture(req.body?.data);
-      if (mailOption) {
-        subject = mailOption.subject;
-        text = mailOption.text;
-        to = mailOption.to;
-      }
-    }
+    console.log('MAIL_HOST: ', process.env.MAIL_HOST);
+    console.log('MAIL_ADDRESS_WEB: ', process.env.MAIL_ADDRESS_WEB);
+    console.log('MAIL_PASS_WEB: ', process.env.MAIL_PASS_WEB);
+    console.log(
+      'MAIL_ADDRESS_HAUTE_COUTURE: ',
+      process.env.MAIL_ADDRESS_HAUTE_COUTURE
+    );
+    console.log('MAIL_ADDRESS_CONTACT: ', process.env.MAIL_ADDRESS_CONTACT);
+    // const transporter = createTransport({
+    //   host: process.env.MAIL_HOST,
+    //   port: 465,
+    //   secure: true,
+    //   auth: {
+    //     user: process.env.MAIL_ADDRESS_WEB,
+    //     pass: process.env.MAIL_PASS_WEB,
+    //   },
+    //   tls: {
+    //     // do not fail on invalid certs
+    //     rejectUnauthorized: false,
+    //   },
+    // });
+    // let subject = 'お問い合わせ';
+    // let text = '';
+    // let to = process.env.MAIL_ADDRESS_CONTACT;
+    // if (req.body?.type === 'haute-couture') {
+    //   const mailOption = hauteCouture(req.body?.data);
+    //   if (mailOption) {
+    //     subject = mailOption.subject;
+    //     text = mailOption.text;
+    //     to = mailOption.to;
+    //   }
+    // }
 
-    const info = await transporter.sendMail({
-      from: process.env.MAIL_ADDRESS_WEB,
-      to,
-      subject,
-      text,
-    });
+    // const info = await transporter.sendMail({
+    //   from: process.env.MAIL_ADDRESS_WEB,
+    //   to,
+    //   subject,
+    //   text,
+    // });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', getTestMessageUrl(info));
+    // console.log('Message sent: %s', info.messageId);
+    // console.log('Preview URL: %s', getTestMessageUrl(info));
 
     res.status(200).json({
       success: true,
