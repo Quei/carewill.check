f158dd54fca094ef0fb295b88f759e7ec90650ce
Quei <github@quei.org>
2021-09-01 11:11:07 +0900

    feat: add mail anchor to thanks message

--- pages/api/contact/make-thanks-message/index.ts
+++ pages/api/contact/make-thanks-message/index.ts
@@ -7,16 +7,16 @@ export const makeThanksMessage = ({ type, locale }: Props) => {
     if (locale === 'ja') {
       return `<ul>
 <li>ご入力ありがとうございました！いただいたe-mail宛に、当社よりメールを自動配信いたします。</li>
-<li>自動配信メールがお手元に届かない場合は${hauteCoutureAddress}までご連絡下さい。</li>
+<li>自動配信メールがお手元に届かない場合は<a href="mailto:${hauteCoutureAddress}">${hauteCoutureAddress}</a>までご連絡下さい。</li>
 <li>その後、ヒアリングを経てデザインや金額は確定されます。</li>
-<li>ご希望の服をより正確に知るために、ご希望の服に近い【参考写真】がございましたら${hauteCoutureAddress}宛に送付いただければ助かります。現在着ているもの、インターネット上にあるものなど何でも構いません。</li>
+<li>ご希望の服をより正確に知るために、ご希望の服に近い【参考写真】がございましたら<a href="mailto:${hauteCoutureAddress}">${hauteCoutureAddress}</a>宛に送付いただければ助かります。現在着ているもの、インターネット上にあるものなど何でも構いません。</li>
 </ul>`;
     } else {
       return `<ul>
 <li>Thank you for your input! You will receive an automatic email from us to the email address you provided.</li>
-<li>If you do not receive the automatic email, please contact ${hauteCoutureAddress}.</li>
+<li>If you do not receive the automatic email, please contact <a href="mailto:${hauteCoutureAddress}">${hauteCoutureAddress}</a>.</li>
 <li>Then, after the hearing, the design and price will be finalized.</li>
-<li>To get a more accurate idea of what you are looking for, it would be helpful if you could send us a [reference photo] that is similar to your desired outfit to ${hauteCoutureAddress}.It can be anything you are currently wearing or anything you can find on the Internet.</li>
+<li>To get a more accurate idea of what you are looking for, it would be helpful if you could send us a [reference photo] that is similar to your desired outfit to <a href="mailto:${hauteCoutureAddress}">${hauteCoutureAddress}</a>.It can be anything you are currently wearing or anything you can find on the Internet.</li>
 </ul>
 `;
     }
