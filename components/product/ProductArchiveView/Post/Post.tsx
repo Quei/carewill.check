bc7815add3492fab65c7b06308ce47adc6a61018
Quei <github@quei.org>
2021-10-29 17:44:26 +0900

    fix: remove makuake link

--- components/product/ProductArchiveView/Post/Post.tsx
+++ components/product/ProductArchiveView/Post/Post.tsx
@@ -57,12 +57,7 @@ const Post: VFC<Props> = ({
         className={cn(s.textBlock)}
         href={href}
       >
-        <TextBlockChildElement>
-          {description}
-          <p>
-            <span className={cn('underline')}>{f('makuakeLink')}</span>
-          </p>
-        </TextBlockChildElement>
+        <TextBlockChildElement>{description}</TextBlockChildElement>
       </Block>
       <div className={cn('relative', 'aspect-w-1', 'aspect-h-1')}>
         <Link href={href}>
